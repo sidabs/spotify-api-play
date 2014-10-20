a979f454cc493584869939034952836883d15d1f
siddhartha dabral <siddhartha.dabral@gmail.com>
2014-10-20 15:08:55 -0400

    Updated Spotify Helper to include generateAuthorizeUri and getApiTokens functions
    Added functions to spotify helper
    Replaced spotify routes file to use new helper functions

--- helpers/spotify.js
+++ helpers/spotify.js
@@ -2,15 +2,63 @@ var webRequest  = require('./request');
 
 var spotify     = {
     config: {
+        clientId:       process.env.CLIENT_ID       || '',
+        clientSecret:   process.env.CLIENT_SECRET   || '',
         api:    {
-            host:           'api.spotify.com',
-            limit:          30
+            host:       'api.spotify.com',
+            limit:      30
         },
         auth:   {
-            host:           'accounts.spotify.com',
-            callbackUri:    'http://localhost:3000/spotify/callback',
-            scope:          'playlist-read-private playlist-modify-private user-read-private'
+            host:       'accounts.spotify.com',
+            scope:      'playlist-read-private playlist-modify-private user-read-private',
+            cbUri:      'http://localhost:3000/spotify/callback'
         }
+    },
+    generateAuthorizeUri:   function() {
+        //required imports
+        var spotify         = require('../helpers/spotify');
+        //Request Specific Variables
+        var responseType    = 'code';
+        var state           = 'profile/activity';
+        //Spotify Request Variables
+        var host            = spotify.config.auth.host;
+        var path            ='/authorize/?client_id=' + encodeURIComponent(spotify.config.clientId)
+                                                        + '&redirect_uri=' + encodeURIComponent(spotify.config.auth.cbUri)
+                                                        + '&scope=' + encodeURIComponent(spotify.config.auth.scope)
+                                                        + '&response_type=' + responseType 
+                                                        + '&state=' + state;
+        return 'https://' + host + path;
+    },
+    getApiTokens:           function(authorizationCode, callback) {
+        //required imports
+        var querystring     = require('querystring');
+        var request         = require('../helpers/request');
+        //Environment Variables Needed
+        //Spotify Request Variabels
+        var host        = spotify.config.auth.host;
+        var path        = '/api/token';
+        var postData    = {
+            'grant_type':       'authorization_code',
+            'code':             authorizationCode,
+            'redirect_uri':     spotify.config.auth.cbUri
+        };
+        //Request Specific Variables
+        var postDataStr = querystring.stringify(postData);
+        //http headers set (including Authorization key/value)
+        var headers     = {
+            "Content-Type":     "application/x-www-form-urlencoded",
+            "Content-Length":   postDataStr.length,
+            "Authorization":    "Basic " + new Buffer(spotify.config.clientId + ":" + spotify.config.clientSecret).toString('base64')      
+        };
+        //Execute Post to get tokens
+        request.executePost(host, path, headers, postDataStr, function(responseStr) {
+            try {
+                var responseJSON    = JSON.parse(responseStr);
+                callback(responseJSON);
+            } catch(err) {
+                callback(err);
+            }
+        });
     }
 };
 
--- routes/spotify.js
+++ routes/spotify.js
@@ -8,22 +8,9 @@ router.get('/', function(req, res) {
 /* Executes Authorization for Initial Setup */
 router.get('/authorize', function(req, res) {
     //required imports
-    var spotify         = require('../helpers/spotify');
-    //Environment Variables Needed
-    var clientId        = process.env.CLIENT_ID     || '';
-    //Request Specific Variables
-    var responseType    = 'code';
-    var redirectUri     = spotify.config.auth.callbackUri;
-    var scope           = spotify.config.auth.scope;
-    var state           = 'profile/activity';
-    //Spotify Request Variables
-    var host            = spotify.config.auth.host;
-    var path            ='/authorize/?client_id=' + encodeURIComponent(clientId)
-                                                    + '&response_type=' + responseType 
-                                                    + '&redirect_uri=' + encodeURIComponent(redirectUri)
-                                                    + '&scope=' + encodeURIComponent(scope)
-                                                    + '&state=' + state;
-    var url             = 'https://' + host + path;
+    var spotify = require('../helpers/spotify');
+    //generate authorization url
+    var url     = spotify.generateAuthorizeUri();
     //Redirect for authorization
     res.redirect(url);
 });
@@ -38,33 +25,18 @@ router.get('/callback', function(req, res) {
         res.send('ERROR: ' + error);
     } else {
         //required imports
-        var querystring     = require('querystring');
-        var request         = require('../helpers/request');
         var spotify         = require('../helpers/spotify');
-        //Environment Variables Needed
-        var clientId        = process.env.CLIENT_ID     || '';
-        var clientSecret    = process.env.CLIENT_SECRET || '';
-        var redirectUri     = spotify.config.auth.callbackUri;
-        //Spotify Request Variabels
-        var host        = spotify.config.auth.host;
-        var path        = '/api/token';
-        var postData    = {
-            'grant_type':       'authorization_code',
-            'code':             code,
-            'redirect_uri':     spotify.config.auth.callbackUri
-        };
-        //Request Specific Variables
-        var postDataStr = querystring.stringify(postData);
-        //http headers set (including Authorization key/value)
-        var headers     = {
-            "Content-Type":     "application/x-www-form-urlencoded",
-            "Content-Length":   postDataStr.length,
-            "Authorization":    "Basic " + new Buffer(clientId + ":" + clientSecret).toString('base64')      
-        };
-        //Execute Post to get tokens
-        request.executePost(host, path, headers, postDataStr, function(responseStr) {
-            console.log('RESPONSE: ', responseStr);
-            res.send(responseStr);
+        spotify.getApiTokens(code, function(result) {
+            if(result instanceof Error) {
+                //currently only occuring if parsing error with response string to json
+                res.json({
+                    error:  'response parsing error'
+                });
+            } else {
+                //currently errors returned are json object (error and error_description)
+                //so no special handling needed at the moment
+                res.json(result);
+            }
         });
     }
 });
